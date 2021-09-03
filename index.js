8c25575d34e654f683e8432977279bfc6b6a4f9a
MartinDK <marty@live.com.au>
2021-09-03 20:35:17 +1000

    Post geolocation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { response } = require('express');
+const Datastore = require('nedb');
 const express = require('express');
 
 // Create server and listen on port 3000
 const app = express();
 app.listen(3000, () => console.log('Listening 3000'));
-
-// Where to look and serve static files from
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 // Post API
 app.post('/api', (request, response) => {
     console.log(request.body);
