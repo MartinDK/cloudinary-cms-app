4a4ae51df7e07f8ea1b1d96a8001cd0ae118711b
MartinDK <marty@live.com.au>
2021-09-03 13:54:03 +1000

    Added post request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 // Create server and listen on port 3000
@@ -6,3 +7,9 @@ app.listen(3000, () => console.log('Listening 3000'));
 
 // Where to look and serve static files from
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+// Post API
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    response.json({ status: 'successful!!!!!' });
+});
